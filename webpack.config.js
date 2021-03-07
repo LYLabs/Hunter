bdbcccd11b8fe840ff70510ac47982e3061d8388
AndreaLi <andrea.li8341@hotmail.com>
2021-03-07 15:28:51 -0800

    add url loader

--- webpack.config.js
+++ webpack.config.js
@@ -36,6 +36,17 @@ module.exports = {
           },
         ],
       },
+      {
+        test: /\.(png|jpg|gif)$/i,
+        use: [
+          {
+            loader: 'url-loader',
+            options: {
+              limit: 8192,
+            },
+          },
+        ],
+      },
     ],
   },
   devServer: {
